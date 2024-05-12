ecea24508f5fdf1729cf8dde4fd2779d76a310e0
Antes <coderasylum@gmail.com>
2024-05-12 20:01:33 +0300

    feat: add prediction entity and module

---
+++ src/prediction/prediction.entity.ts
@@ -0,0 +1,18 @@
+import { Observation } from "src/observation/observation.entity";
+import { Species } from "src/species/species.entity";
+import { Entity, Column, Relation, PrimaryGeneratedColumn, ManyToOne } from "typeorm";
+
+@Entity()
+export class Prediction {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @ManyToOne(() => Observation, (observation) => observation.predictions)
+    observation: Relation<Observation>;
+
+    @ManyToOne(() => Species, (species) => species.predictions)
+    species: Relation<Species>;
+
+    @Column('decimal', {precision: 4, scale: 2})
+    probability: number;
+}
