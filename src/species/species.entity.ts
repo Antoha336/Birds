2dff5086796bf3793b002fc4b7ed8b778c493b85
Antes <coderasylum@gmail.com>
2024-05-12 20:01:50 +0300

    feat: add species entity and module

---
+++ src/species/species.entity.ts
@@ -0,0 +1,21 @@
+import { Entity, Column, Relation, PrimaryGeneratedColumn, ManyToOne, OneToMany } from "typeorm";
+import { Order } from "src/order/order.entity";
+import { Prediction } from "src/prediction/prediction.entity";
+
+@Entity()
+export class Species {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    latin_name: string;
+
+    @Column({nullable: true})
+    russian_name: string;
+
+    @ManyToOne(() => Order, (order) => order.order_species)
+    order: Relation<Order>;
+
+    @OneToMany(() => Prediction, (prediction) => prediction.id)
+    predictions: Relation<Prediction>[];
+}
