56b700490bf465c9a5be7d7bc2b6d8b68f3ea423
Antes <coderasylum@gmail.com>
2024-05-14 23:26:33 +0300

    chorus: add admin requirement to order endpoints

--- src/order/order.controller.ts
+++ src/order/order.controller.ts
@@ -2,6 +2,7 @@ import { Body, Controller, Delete, HttpCode, HttpStatus, Param, Post, UseGuards 
 import { AuthGuard } from 'src/auth/auth.guard';
 import { CreateOrderDto } from './dto/create-order.dto';
 import { OrderService } from './order.service';
+import { AdminOnly } from 'src/auth/auth.decorator';
 
 @Controller('order')
 @UseGuards(AuthGuard)
@@ -9,12 +10,14 @@ export class OrderController {
   constructor(private orderService: OrderService) {}
 
   @Post()
+  @AdminOnly()
   @HttpCode(HttpStatus.CREATED)
   create(@Body() createOrderDto: CreateOrderDto): Promise<void> {
     return this.orderService.create(createOrderDto);
   }
 
   @Delete(':id')
+  @AdminOnly()
   @HttpCode(HttpStatus.NO_CONTENT)
   remove(@Param('id') id: number): Promise<void> {
     return this.orderService.remove(id);
